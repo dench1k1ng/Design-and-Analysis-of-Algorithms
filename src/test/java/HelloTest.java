05f0f411b832123d2af3d7692667420da5bfccaa
dench1k1ng <deniskashin1004@gmail.com>
2025-10-01 18:04:17 +0500

    init: junit

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloTest {
+    @Test
+    void testSum() {
+        assertEquals(5, 2 + 3);
+    }
+}
